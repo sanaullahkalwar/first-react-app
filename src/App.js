76a82417def156dd8856af30be7356de93800bb4
sanaullahkalwar <sanullahkalwar@gmail.com>
2020-06-08 15:26:58 +0500

    update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         SAnaullah kalwar
+        from Dist Ghotki
         </p>
         <a
           className="App-link"
