6f52c6669d89171376825f12bd752928d95d51ff
sanaullahkalwar <sanullahkalwar@gmail.com>
2020-06-09 07:55:33 +0500

    set the tags

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         SAnaullah kalwar
-        from Dist Ghotki<br><p> Educated From University of sindh</p>
-        </br>
+        from Dist Ghotki Educated From University of sindh
         </p>
         <a  
           className="App-link"
